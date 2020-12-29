5d98e3dba2534fbd2083b0dadeb54d231c1fdc88
U-JPL\mmedrano <mmedrano@MT-202935.jpl.nasa.gov>
2020-12-29 09:54:55 -0800

    Commit: Removed the ZCU102 interface program.  Too large 	for the repo.

--- zcu102_scui/flash_restore/tests/ZCU102/bat/firmware_flash_gui_070517.bat
+++
@@ -1,4 +0,0 @@
-:: Note: change the "CP201x" to match what your driver names the ports, e.g. CP2105, CP2108, or CP210x.
-@ECHO OFF
-cd bat
-MSP_BSL_Flasher.exe -f "..\elf\MSP_code_070517.txt" -c "Silicon Labs Quad CP210x USB to UART Bridge: Interface 0" -v -p
